2b037f71c1bb4752b38d28533a18b003d3d170d3
James Wilson <jwilson1@fossil.com>
2013-12-31 11:06:42 -0600

    Dropping the linkedin resume idea

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
 var core = (function () {
-
-	var fetchSuccess = function () {
-		console.log('fetch args', arguments);
-	};
-
 	return {
-		init: function () {
-			IN.API.Profile('37178410').result(fetchSuccess);
-		}
+		init: function () {}
 	};
 })();
+
+core.init();
