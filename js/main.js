e571f4c0db7727b8de4e587c67472fc4392dd84e
James Wilson <jwilson1@fossil.com>
2013-12-31 14:41:05 -0600

    Some more changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,38 @@
+/* global google: false */
 var core = (function () {
-	return {
-		init: function () {}
-	};
+
+  var buildSkillsGraph = function () {
+    google.load('visualization', '1', { packages:['corechart'] });
+    google.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ['Skill',          'Efficiency'],
+        ['Client JS',       9.5],
+        ['Node.js',         7],
+        ['HTML/CSS',        9.5],
+        ['OOP PHP',         8.5],
+        ['ActionScript 3',  6],
+        ['C#/.NET',         5],
+        ['MySQL',           7]
+      ]),
+
+      options = {
+        // title: 'Test',
+        legend: { position: 'none' }
+      },
+
+      chart = new google.visualization.BarChart(document.getElementById('skills-graph'));
+
+      chart.draw(data, options);
+    }
+  };
+
+  return {
+    init: function () {
+      buildSkillsGraph();
+    }
+  };
 })();
 
-core.init();
+// core.init();
