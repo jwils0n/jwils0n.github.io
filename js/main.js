4d5d8416475d0f8611d1dfee7dabe6f26049b4f0
James Wilson <jwilson1@fossil.com>
2013-12-31 10:21:51 -0600

    more testing

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-function run () {
-	console.log('args', arguments);
-	console.log('in', IN);
-}
+var core = (function () {
+
+	var fetchSuccess = function () {
+		console.log('fetch args', arguments);
+	};
+
+	return {
+		init: function () {
+			IN.API.Profile(37178410).result(fetchSuccess);
+		}
+	};
+})();
